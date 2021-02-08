ae90b5e1a3ba6453617fa577da221a35cef743d7
Bizikin <robi90@mail.ru>
2021-02-08 03:18:37 +0300

    Правки ко второму заданию v6

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
+'use strict'
+
 let getRandomInt = function(min, max) {
-  return getRandomFloat(min,max,0);
+  return getRandomFloat(min, max, 0);
 }
 
 let getRandomFloat = function(min, max, countComma) {
-  if (max>min && min>=0) {
-  return parseFloat(((Math.random() * (max - min)) + min).toFixed(countComma)); 
+  if (max > min && min >= 0) {
+    return parseFloat(((Math.random() * (max - min)) + min).toFixed(countComma));
+  } else {
+    alert('Ошибка, значение минимальной переменной не должно быть отрицательным, больше максимального или равным ему');
   }
-  else {
-    console.log('Ошибка, значение минимальной переменной не должно быть отрицательным, больше максимального или равным ему');
-    }
 }
 
-getRandomInt(min,max);
-getRandomFloat(min,max,countComma);
-console.log('Полученное значение: '+getRandInt(2.6,3.5,0));
+getRandomInt(min, max);
+getRandomFloat(min, max, countComma);
